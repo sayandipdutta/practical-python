09fa143a6a9aaa3c6a8be3b014b121437a7a2709
sayandip199309@gmail.com <sayandip199309@gmail.com>
2022-02-08 23:51:35 +0530

    Solve mortgage.py, pcost.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,52 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment_start, extra_payment_end = 61, 108
+
+def duration(principal,
+             rate,
+             payment,
+             *,
+             extra_payment=0,
+             correct_overpaymnet=False):
+    months_paid = 0
+    total_paid = 0.00
+    while principal > 0:
+        if extra_payment_start <= months_paid <= extra_payment_end:
+            monthly_payment = payment + extra_payment
+        else:
+            monthly_payment = payment
+        principal = principal * (1 + rate / 12)
+        if correct_overpaymnet and (principal - monthly_payment < 0):
+            monthly_payment = principal
+        principal -= monthly_payment
+        total_paid += monthly_payment
+        months_paid += 1
+        print(months_paid, total_paid, principal)
+    return months_paid, total_paid
+
+
+months_paid, total_paid = duration(principal, rate, payment)
+print(f"Without extra payments: {months_paid=}, {total_paid=}")
+
+months_paid, total_paid = duration(principal,
+                                   rate,
+                                   payment,
+                                   extra_payment=1000.00)
+print(f"With extra payments: {months_paid=}, {total_paid=}")
+
+months_paid, total_paid = duration(principal,
+                                   rate,
+                                   payment,
+                                   correct_overpaymnet=True)
+print(f"Without extra payments, and correction: {months_paid=}, {total_paid=}")
+
+months_paid, total_paid = duration(principal,
+                                   rate,
+                                   payment,
+                                   extra_payment=1000.00,
+                                   correct_overpaymnet=True)
+print(f"With extra payments, and correction: {months_paid=}, {total_paid=}")
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,31 @@
 # pcost.py
 #
 # Exercise 1.27
+from os import PathLike
+from pathlib import Path
+from typing import Iterator
+
+def iterate_lines(file_it: Iterator):
+    for line in file_it:
+        if line := line.strip():
+            yield line
+
+
+def calculate_tot_value(datafile: PathLike[str]):
+    total_price = 0.00
+    with open(datafile, "rt") as f:
+        headers = next(f)
+        print(*headers.split(','), sep='\t')
+        for line in iterate_lines(f):
+            name, shares, price = line.split(",")
+            print(name, shares, price, sep='\t')
+            shares, price = float(shares), float(price)
+            stock_value = shares * price
+            total_price += stock_value
+    return total_price
+
+
+if __name__ == "__main__":
+    DATAFILE: PathLike[str] = Path("Data/portfolio.csv")
+    total_price = calculate_tot_value(DATAFILE)
+    print("Total price:", total_price)
